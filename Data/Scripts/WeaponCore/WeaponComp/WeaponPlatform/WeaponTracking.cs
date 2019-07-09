c9585be46c2fa66d107b0423fe57dba9d069f3ed
Shane Stixrud <shane@geeklords.org>
2019-07-09 16:47:02 -0700

    dsadsa

--- Data/Scripts/WeaponCore/WeaponComp/WeaponPlatform/WeaponTracking.cs
+++ Data/Scripts/WeaponCore/WeaponComp/WeaponPlatform/WeaponTracking.cs
@@ -112,7 +112,7 @@ namespace WeaponCore.Platform
 
             var alignedChange = wasAligned != isAligned;
             if (alignedChange && isAligned) weapon.StartShooting();
-            else if (alignedChange) weapon.StopShooting();
+            else if (alignedChange && !weapon.DelayCeaseFire) weapon.StopShooting();
             weapon.Comp.TurretTargetLock = weapon.IsTracking && weapon.IsInView && weapon.IsAligned;
             return weapon.IsTracking;
         }
