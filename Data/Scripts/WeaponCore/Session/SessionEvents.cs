958ef13e127e4f136ebc4ff6ab95e61af3c58f4e
Shane Stixrud <shane@geeklords.org>
2019-08-25 18:59:53 -0700

    dsadsa

--- Data/Scripts/WeaponCore/Session/SessionEvents.cs
+++ Data/Scripts/WeaponCore/Session/SessionEvents.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using Sandbox.Common.ObjectBuilders;
-using Sandbox.Definitions;
 using Sandbox.Game.Entities;
-using Sandbox.Game.Gui;
-using Sandbox.Game.Screens.Helpers;
 using Sandbox.ModAPI;
-using Sandbox.ModAPI.Interfaces.Terminal;
-using Sandbox.Game.Weapons;
-using Sandbox.ModAPI.Weapons;
 using SpaceEngineers.Game.ModAPI;
-using VRage.Game;
 using VRage.Game.Entity;
-using VRage.Game.ModAPI;
-using VRageMath;
-using WeaponCore.Data.Scripts.WeaponCore.Support;
 using WeaponCore.Support;
 
 namespace WeaponCore
