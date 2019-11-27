d454cdaa9519bbab7c45ef35bd9e795f791c926d
blitz2190 <blitz2190@DESKTOP-216274I>
2019-11-26 20:56:33 -0500

    asdasd

--- Data/Scripts/WeaponCore/WeaponThread/MasterConfig.cs
+++ Data/Scripts/WeaponCore/WeaponThread/MasterConfig.cs
@@ -7,8 +7,8 @@
             // file convention: Name.cs - See Example.cs file for weapon property details.
             //
             // Enable your config files using the follow syntax, don't include the ".cs" extension:
-            // ConfigFiles(Your1stConfigFile, Your2ndConfigFile, Your3rdConfigFile);
-            ConfigFiles(LargeGatlingTurret, 
+            ConfigFiles();
+            /*ConfigFiles(LargeGatlingTurret, 
                         LargeMissileTurret, 
                         SmallMissileTurret, 
                         LargeMissileLauncher,
@@ -17,6 +17,7 @@
                         SmallMissileLauncher,
                         SmallGatlingTurret,
                         SmallRocketLauncherReload); //SmallGatlingGun LargeGatlingTurret
+                        */
         }
     }
 }
