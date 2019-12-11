e342820b9bdf2b45a77af11bd1c3c7a548779ac6
blitz2190 <blitz2190@DESKTOP-216274I>
2019-12-11 00:36:49 -0500

    asdasd

--- Data/Scripts/WeaponCore/Api/ApiBackend.cs
+++ Data/Scripts/WeaponCore/Api/ApiBackend.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 using Sandbox.ModAPI;
 using VRage;
 using VRage.Collections;
+using VRage.Game;
 using VRage.Game.Entity;
+using VRage.ModAPI;
 using VRageMath;
+using static WeaponCore.Support.TargetingDefinition;
 
 namespace WeaponCore.Support
 {
@@ -15,14 +18,39 @@ namespace WeaponCore.Support
     {
         internal readonly Dictionary<string, Delegate> ModApiMethods = new Dictionary<string, Delegate>()
         {
-            ["MatchEntToShieldFastExt"] =
-                new Func<MyEntity, bool, MyTuple<IMyTerminalBlock, MyTuple<bool, bool, float, float, float, int>, MyTuple<MatrixD, MatrixD>>?>(TAPI_MatchEntToShieldFastExt),
+            ["GetCoreStaticLaunchers"]= new Func<List<MyDefinitionId>>(GetCoreStaticLaunchers),
+            ["GetCoreTurrets"] = new Func<List<MyDefinitionId>>(GetCoreTurrets),
+            ["SetTargetEntity"] = new Action<IMyEntity, IMyEntity>(SetTargetEntity),
+            ["FireOnce"] = new Action<IMyTerminalBlock>(FireOnce),
+            ["ToggleFire"] = new Action<IMyTerminalBlock, bool>(ToggleFire),
+            ["WeaponReady"] = new Func<IMyTerminalBlock, bool?>(WeaponReady),
+            ["GetMaxRange"] = new Func<IMyTerminalBlock, float?>(GetMaxRange),
+            ["GetTurretTargetTypes"] = new Func<IMyTerminalBlock, List<List<Threat>>>(GetTurretTargetTypes),
+            ["SetTurretTargetTypes"] = new Action<IMyTerminalBlock, List<List<Threat>>>(SetTurretTargetTypes),
+            ["SetTurretRange"] = new Action<IMyTerminalBlock, float>(SetTurretRange),            
+            ["GetTargetedEntity"] = new Func<IMyTerminalBlock, IMyEntity>(GetTargetedEntity),
+            ["TargetPredictedPosition"] = new Func<IMyTerminalBlock, Vector3D?>(TargetPredictedPosition),
+            ["GetHeatLevel"] = new Func<IMyTerminalBlock, float?>(GetHeatLevel),
+            ["CurrentPower"] = new Func<IMyTerminalBlock, float?>(CurrentPower),
+            ["MaxPower"] = new Func<MyDefinitionId, float?>(MaxPower),
+            ["DisableRequiredPower"] = new Action<IMyTerminalBlock>(DisableRequiredPower)
         };
 
         private readonly Dictionary<string, Delegate> _terminalPbApiMethods = new Dictionary<string, Delegate>()
         {
-            ["MatchEntToShieldFastExt"] =
-                new Func<MyEntity, bool, MyTuple<IMyTerminalBlock, MyTuple<bool, bool, float, float, float, int>, MyTuple<MatrixD, MatrixD>>?>(TAPI_MatchEntToShieldFastExt),
+            ["SetTargetEntity"] = new Action<IMyEntity, IMyEntity>(SetTargetEntity),
+            ["FireOnce"] = new Action<IMyTerminalBlock>(FireOnce),
+            ["ToggleFire"] = new Action<IMyTerminalBlock, bool>(ToggleFire),
+            ["WeaponReady"] = new Func<IMyTerminalBlock, bool?>(WeaponReady),
+            ["GetMaxRange"] = new Func<IMyTerminalBlock, float?>(GetMaxRange),
+            ["GetTurretTargetTypes"] = new Func<IMyTerminalBlock, List<List<Threat>>>(GetTurretTargetTypes),
+            ["SetTurretTargetTypes"] = new Action<IMyTerminalBlock, List<List<Threat>>>(SetTurretTargetTypes),
+            ["SetTurretRange"] = new Action<IMyTerminalBlock, float>(SetTurretRange),
+            ["GetTargetedEntity"] = new Func<IMyTerminalBlock, IMyEntity>(GetTargetedEntity),
+            ["TargetPredictedPosition"] = new Func<IMyTerminalBlock, Vector3D?>(TargetPredictedPosition),
+            ["GetHeatLevel"] = new Func<IMyTerminalBlock, float?>(GetHeatLevel),
+            ["CurrentPower"] = new Func<IMyTerminalBlock, float?>(CurrentPower),
+            ["MaxPower"] = new Func<MyDefinitionId, float?>(MaxPower)
         };
 
         internal void Init()
@@ -36,10 +64,84 @@ namespace WeaponCore.Support
             MyAPIGateway.TerminalControls.AddControl<Sandbox.ModAPI.Ingame.IMyProgrammableBlock>(pb);
         }
 
-        private static MyTuple<IMyTerminalBlock, MyTuple<bool, bool, float, float, float, int>, MyTuple<MatrixD, MatrixD>>? TAPI_MatchEntToShieldFastExt(MyEntity entity, bool onlyIfOnline)
+        private static List<MyDefinitionId> GetCoreStaticLaunchers()
         {
-            if (entity == null) return null;
+            return new List<MyDefinitionId>();
+        }
+
+        private static List<MyDefinitionId> GetCoreTurrets()
+        {
+            return new List<MyDefinitionId>();
+        }
+
+        private static void SetTargetEntity(IMyEntity shooter, IMyEntity target)
+        {
+
+        }
+
+        private static void FireOnce(IMyTerminalBlock weaponBlock)
+        {
+
+        }
+
+        private static void ToggleFire(IMyTerminalBlock weaponBlock, bool on)
+        {
+
+        }
+
+        private static bool? WeaponReady(IMyTerminalBlock weaponBlock)
+        {
+            return false;
+        }
+
+        private static float? GetMaxRange(IMyTerminalBlock weaponBlock)
+        {
+            return 0f;
+        }
+
+        private static List<List<Threat>> GetTurretTargetTypes(IMyTerminalBlock weaponBlock)
+        {
+            return new List<List<Threat>>();
+        }
+
+        private static void SetTurretRange(IMyTerminalBlock weaponBlock, float range)
+        {
+
+        }
+
+        private static void SetTurretTargetTypes(IMyTerminalBlock weaponBlock, List<List<Threat>> threats)
+        {
+
+        }
+
+        private static IMyEntity GetTargetedEntity(IMyTerminalBlock weaponBlock)
+        {
             return null;
+        }
+
+        private static Vector3D? TargetPredictedPosition(IMyTerminalBlock weaponBlock)
+        {
+            return Vector3D.Zero;
+        }
+
+        private static float? GetHeatLevel(IMyTerminalBlock weaponBlock)
+        {
+            return 0f;
+        }
+
+        private static float? CurrentPower(IMyTerminalBlock weaponBlock)
+        {
+            return 0f;
+        }
+
+        private static float? MaxPower(MyDefinitionId weaponBlock)
+        {
+            return 0f;
+        }
+
+        private static void DisableRequiredPower(IMyTerminalBlock weaponBlock)
+        {
+
         }
     }
 }
