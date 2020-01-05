1325fb6046ff70955cbb89805d8d254b58f69100
Shane Stixrud <shane@geeklords.org>
2020-01-04 18:25:41 -0800

    dsdsa

--- Data/Scripts/WeaponCore/WeaponComp/WeaponPlatform/WeaponShoot.cs
+++ Data/Scripts/WeaponCore/WeaponComp/WeaponPlatform/WeaponShoot.cs
@@ -23,7 +23,7 @@ namespace WeaponCore.Platform
             var userControlled = Comp.Gunner || state.ManualShoot != TerminalActionState.ShootOff;
             var targetable = System.Values.Ammo.Health > 0 && !System.IsBeamWeapon;
 
-            Log.Line($"shootDelay:{tick - ShootDelayTick} - untilShoot:{_ticksUntilShoot} - shootTick:{_shootTick} - TicksPerShot:{TicksPerShot}");
+            if (System.WeaponName == "Gatling") Log.Line($"{System.WeaponName} - tick - ShootDelayTick:{tick - ShootDelayTick} - tickUntilShoot:{_ticksUntilShoot} - shootTick:{_shootTick} - TicksPerShot:{TicksPerShot}");
             if (ShootDelayTick > tick) return;
 
             //if (ShotCounter == 0 && _newCycle)
