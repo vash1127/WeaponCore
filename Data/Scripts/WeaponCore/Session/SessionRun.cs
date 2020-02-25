44b420c21976593ee51b2241e4804c6a913a5a07
Shane Stixrud <shane@geeklords.org>
2020-02-24 20:25:26 -0800

    dsada

--- Data/Scripts/WeaponCore/Session/SessionRun.cs
+++ Data/Scripts/WeaponCore/Session/SessionRun.cs
@@ -281,7 +281,7 @@ namespace WeaponCore
                 foreach (var mod in Session.Mods)
                 {
                     if (mod.PublishedFileId == 1365616918) ShieldMod = true;
-                    else if (mod.PublishedFileId == 1931509062 || mod.PublishedFileId == 1995197719) ReplaceVanilla = true;
+                    else if (mod.PublishedFileId == 1931509062 || mod.PublishedFileId == 1995197719 || mod.PublishedFileId == 2006751214) ReplaceVanilla = true;
                     else if (mod.GetPath().Contains("AppData\\Roaming\\SpaceEngineers\\Mods\\VanillaReplacement"))
                         ReplaceVanilla = true;
                 }
