c9f10283ac25101f25b6b76e16c2b27e55ad428c
Shane Stixrud <shane@geeklords.org>
2020-02-25 21:18:20 -0800

    dsadsa

--- Data/Scripts/WeaponCore/Session/SessionNetwork.cs
+++ Data/Scripts/WeaponCore/Session/SessionNetwork.cs
@@ -441,7 +441,7 @@ namespace WeaponCore
                             break;
                         }
                     default:
-                        Reporter.ReportData[packet.PType].Add(report);
+                        Reporter.ReportData[PacketType.Invalid].Add(report);
                         report.PacketValid = false;
 
                         break;
