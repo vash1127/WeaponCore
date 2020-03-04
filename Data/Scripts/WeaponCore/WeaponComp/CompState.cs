297a3a6443d8b89534f5b5963399872fc99ec72a
Shane Stixrud <shane@geeklords.org>
2020-03-04 14:38:08 -0800

    dsadsa

--- Data/Scripts/WeaponCore/WeaponComp/CompState.cs
+++ Data/Scripts/WeaponCore/WeaponComp/CompState.cs
@@ -12,9 +12,11 @@ namespace WeaponCore.Support
             switch (Status)
             {
                 case Start.Starting:
+                    Log.Line("HealthCheck Starting");
                     Startup();
                     break;
                 case Start.ReInit:
+                    Log.Line("HealthCheck Reinit");
                     Platform.ResetParts(this);
                     Status = Start.Started;
                     break;
