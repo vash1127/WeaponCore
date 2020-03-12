758cc8586037735f310b02265ae61beebac278fe
blitz2190 <blitz2190@DESKTOP-216274I>
2020-03-12 14:49:54 -0400

    asdasd

--- Data/Scripts/WeaponCore/Api/WeaponCoreApi.cs
+++ Data/Scripts/WeaponCore/Api/WeaponCoreApi.cs
@@ -69,7 +69,7 @@ namespace WeaponCore.Api
         /// </summary>
         /// <param name="getWeaponDefinitions">Set to true to fill <see cref="WeaponDefinitions"/>.</param>
         /// <returns>True if WeaponCore replied.</returns>
-        public bool Load(bool getWeaponDefinitions = false)
+        public void Load(bool getWeaponDefinitions = false)
         {
             _getWeaponDefinitions = getWeaponDefinitions;
 
@@ -79,8 +79,6 @@ namespace WeaponCore.Api
             _isRegistered = true;
             MyAPIGateway.Utilities.RegisterMessageHandler(Channel, HandleMessage);
             MyAPIGateway.Utilities.SendModMessage(Channel, "ApiEndpointRequest");
-            MyAPIGateway.Utilities.UnregisterMessageHandler(Channel, HandleMessage);
-            return IsReady;
         }
 
         public void Unload()
@@ -103,6 +101,7 @@ namespace WeaponCore.Api
 
             IsReady = true;
             ApiAssign(dict, _getWeaponDefinitions);
+            MyAPIGateway.Utilities.UnregisterMessageHandler(Channel, HandleMessage);
         }
 
 
