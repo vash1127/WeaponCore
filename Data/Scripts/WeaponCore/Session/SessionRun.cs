e0a04e678007385633c281d32c5c997d2387aa17
Shane Stixrud <shane@geeklords.org>
2020-03-15 12:03:35 -0700

    dsdsa

--- Data/Scripts/WeaponCore/Session/SessionRun.cs
+++ Data/Scripts/WeaponCore/Session/SessionRun.cs
@@ -121,8 +121,8 @@ namespace WeaponCore
                 if (FragmentsNeedingEntities.Count > 0)
                     Projectiles.PrepFragmentEntities();
 
-                //PTask = MyAPIGateway.Parallel.StartBackground(Projectiles.Update);
-                //if (WeaponsToSync.Count > 0) NTask = MyAPIGateway.Parallel.StartBackground(Proccessor.Proccess);
+                PTask = MyAPIGateway.Parallel.Start(Projectiles.Update);
+                if (WeaponsToSync.Count > 0) NTask = MyAPIGateway.Parallel.Start(Proccessor.Proccess);
             }
             catch (Exception ex) { Log.Line($"Exception in SessionSim: {ex}"); }
         }
@@ -135,14 +135,12 @@ namespace WeaponCore
                 if (!DedicatedServer) ProcessAnimations();
 
                 DsUtil.Start("projectiles");
-                /*
+
                 if (!PTask.IsComplete)
                     PTask.Wait();
 
                 if (PTask.IsComplete && PTask.valid && PTask.Exceptions != null)
                     TaskHasErrors(ref PTask, "PTask");
-                    */
-                Projectiles.Update();
 
                 DsUtil.Complete("projectiles", true);
 
@@ -159,7 +157,7 @@ namespace WeaponCore
                 if (Hits.Count > 0) ProcessHits();
                 DsUtil.Complete("damage", true);
 
-                /*
+                DsUtil.Start("network");
                 if (!NTask.IsComplete)
                     NTask.Wait();
 
@@ -168,12 +166,10 @@ namespace WeaponCore
 
                 if (!MTask.IsComplete)
                     MTask.Wait();
-                */
+
                 if (MTask.IsComplete && MTask.valid && MTask.Exceptions != null)
                     TaskHasErrors(ref MTask, "MTask");
-                DsUtil.Start("network");
 
-                if (WeaponsToSync.Count > 0) Proccessor.Proccess();
 
                 Proccessor.AddPackets();
 
