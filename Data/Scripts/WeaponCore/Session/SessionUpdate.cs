68572543e24f50b8f4f572afa813ad2bd7116c8c
blitz2190 <blitz2190@DESKTOP-216274I>
2020-03-20 17:44:31 -0400

    asdasd

--- Data/Scripts/WeaponCore/Session/SessionUpdate.cs
+++ Data/Scripts/WeaponCore/Session/SessionUpdate.cs
@@ -189,7 +189,7 @@ namespace WeaponCore
                                 /// 
                                 w.AiShooting = (w.Target.TargetLock || w.System.DelayCeaseFire && !w.Target.IsAligned && Tick - w.CeaseFireDelayTick <= w.System.CeaseFireDelay) && !comp.UserControlled;
                                 var reloading = (!w.ActiveAmmoDef.AmmoDef.Const.EnergyAmmo || w.ActiveAmmoDef.AmmoDef.Const.MustCharge) && (w.State.Sync.Reloading || w.OutOfAmmo);
-                                var canShoot = !w.State.Sync.Overheated && !reloading && !w.System.DesignatorWeapon;
+                                var canShoot = !w.State.Sync.Overheated && !reloading;
                                 var fakeTarget = overRides.TargetPainter && comp.TrackReticle && w.Target.IsFakeTarget && w.Target.IsAligned;
                                 var validShootStates = fakeTarget || w.State.ManualShoot == ShootOn || w.State.ManualShoot == ShootOnce || w.AiShooting && w.State.ManualShoot == ShootOff;
                                 var manualShot = (compCurPlayer.ControlType == ControlType.Camera || (overRides.ManualControl && comp.TrackReticle) || w.State.ManualShoot == ShootClick) && !gridAi.SupressMouseShoot && (j % 2 == 0 && leftClick || j == 1 && rightClick);
