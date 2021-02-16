ded5c7f4e6570bf4070c3ae688a0d85d7f69b684
Shane Stixrud <shane@geeklords.org>
2021-02-15 23:41:28 -0800

    fix noowner issue

--- Data/Scripts/CoreSystems/Ai/AiDatabase.cs
+++ Data/Scripts/CoreSystems/Ai/AiDatabase.cs
@@ -324,8 +324,7 @@ namespace CoreSystems.Support
                     var bigOwners = grid.BigOwners;
                     var topOwner = bigOwners.Count > 0 ? bigOwners[0] : long.MaxValue;
 
-                    relationship = topOwner != long.MinValue ? MyIDModule.GetRelationPlayerBlock(gridOwner, topOwner, MyOwnershipShareModeEnum.Faction) : MyRelationsBetweenPlayerAndBlock.NoOwnership;
-
+                    relationship = topOwner != long.MaxValue ? MyIDModule.GetRelationPlayerBlock(gridOwner, topOwner, MyOwnershipShareModeEnum.Faction) : MyRelationsBetweenPlayerAndBlock.NoOwnership;
                     var type = grid.GridSizeEnum != MyCubeSize.Small ? MyDetectedEntityType.LargeGrid : MyDetectedEntityType.SmallGrid;
                     entInfo = new MyDetectedEntityInfo(grid.EntityId, string.Empty, type, null, MatrixD.Zero, Vector3.Zero, relationship, new BoundingBoxD(), Session.Tick);
                     return true;
