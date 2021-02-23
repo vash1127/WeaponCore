ef9bb451286e2924b78df2dc47ca568062d5321e
Shane Stixrud <shane@geeklords.org>
2021-02-22 18:48:22 -0800

    fix issue with it shooting through friendlies... another corner case

--- Data/Scripts/WeaponCore/GridAi/AiTargeting.cs
+++ Data/Scripts/WeaponCore/GridAi/AiTargeting.cs
@@ -487,22 +487,19 @@ namespace WeaponCore.Support
                     if (hitInfo?.HitEntity == null || hitInfo.HitEntity is MyVoxelBase)
                         continue;
 
-                    var hitGrid = hitInfo.HitEntity as MyCubeGrid;
+                    var hitEnt = hitInfo.HitEntity?.GetTopMostParent() as MyEntity;
+                    var hitGrid = hitEnt as MyCubeGrid;
                     if (hitGrid != null)
                     {
                         if (hitGrid.MarkedForClose || (hitGrid != block.CubeGrid && hitGrid.IsSameConstructAs(ai.MyGrid)) || !hitGrid.DestructibleBlocks || hitGrid.Immune || hitGrid.GridGeneralDamageModifier <= 0) continue;
-                        bool enemy;
-
+                        var isTarget = hitGrid == block.CubeGrid || hitGrid.IsSameConstructAs(block.CubeGrid);
+                        
                         var bigOwners = hitGrid.BigOwners;
-                        if (bigOwners.Count == 0) enemy = true;
-                        else
-                        {
-                            var relationship = info.EntInfo.Relationship;
-                            enemy = relationship != MyRelationsBetweenPlayerAndBlock.Owner &&
-                                    relationship != MyRelationsBetweenPlayerAndBlock.FactionShare && relationship != MyRelationsBetweenPlayerAndBlock.Friends;
-                        }
-
-                        if (!enemy)
+                        var noOwner = bigOwners.Count == 0;
+                        
+                        TargetInfo otherInfo;
+                        var validTarget = isTarget || noOwner || ai.Targets.TryGetValue(hitEnt, out otherInfo) && (otherInfo.EntInfo.Relationship == MyRelationsBetweenPlayerAndBlock.Enemies || otherInfo.EntInfo.Relationship == MyRelationsBetweenPlayerAndBlock.Neutral || otherInfo.EntInfo.Relationship == MyRelationsBetweenPlayerAndBlock.NoOwnership);
+                        if (!validTarget)
                             continue;
                     }
 
@@ -588,18 +585,14 @@ namespace WeaponCore.Support
                                     if (hitGrid != null) {
 
                                         if (hitGrid.MarkedForClose || hitGrid != cube.CubeGrid && (hitGrid.IsSameConstructAs(ai.MyGrid) || !hitGrid.DestructibleBlocks || hitGrid.Immune || hitGrid.GridGeneralDamageModifier <= 0)) continue;
-                                        bool enemy;
-
                                         var bigOwners = hitGrid.BigOwners;
-
-                                        if (bigOwners.Count == 0) enemy = true;
-                                        else {
-                                            var relationship = info.EntInfo.Relationship;
-                                            enemy = relationship != MyRelationsBetweenPlayerAndBlock.Owner && relationship != MyRelationsBetweenPlayerAndBlock.FactionShare && relationship != MyRelationsBetweenPlayerAndBlock.Friends;
-                                        }
-
-                                        if (!enemy)
+                                        TargetInfo otherInfo;
+                                        var isTarget = hitGrid == cube.CubeGrid || hitGrid.IsSameConstructAs(cube.CubeGrid);
+                                        var noOwner = bigOwners.Count == 0;
+                                        var validTarget = isTarget || noOwner || ai.Targets.TryGetValue(hitEnt, out otherInfo) && (otherInfo.EntInfo.Relationship == MyRelationsBetweenPlayerAndBlock.Enemies || otherInfo.EntInfo.Relationship == MyRelationsBetweenPlayerAndBlock.Neutral || otherInfo.EntInfo.Relationship == MyRelationsBetweenPlayerAndBlock.NoOwnership);
+                                        if (!validTarget)
                                             continue;
+
                                         bestTest = true;
                                     }
                                 }
