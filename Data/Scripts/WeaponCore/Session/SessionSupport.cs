a8037bab3b91702044ca4b63ff0a66a182febfab
Shane Stixrud <shane@geeklords.org>
2021-06-26 17:26:22 -0700

    sync

--- Data/Scripts/WeaponCore/Session/SessionSupport.cs
+++ Data/Scripts/WeaponCore/Session/SessionSupport.cs
@@ -714,14 +714,14 @@ namespace WeaponCore
                 if (mod.PublishedFileId == 1918681825 || mod.PublishedFileId == 2189703321 || mod.PublishedFileId == 2496225055)
                     validId = true;
 
-                if (mod.PublishedFileId == 1365616918 || mod.PublishedFileId == 2189703321) ShieldMod = true;
+                if (mod.PublishedFileId == 1365616918 || mod.PublishedFileId == 2372872458) ShieldMod = true;
                 else if (mod.GetPath().Contains("AppData\\Roaming\\SpaceEngineers\\Mods\\DefenseShields"))
                     ShieldMod = true;
                 else if (mod.PublishedFileId == 1931509062 || mod.PublishedFileId == 1995197719 || mod.PublishedFileId == 2006751214 || mod.PublishedFileId == 2015560129)
                     ReplaceVanilla = true;
                 else if (mod.GetPath().Contains("AppData\\Roaming\\SpaceEngineers\\Mods\\VanillaReplacement") || mod.Name.StartsWith("WCVanilla"))
                     ReplaceVanilla = true;
-                else if (mod.PublishedFileId == 2123506303 || mod.PublishedFileId == 2496225055)
+                else if (mod.PublishedFileId == 2189703321 || mod.PublishedFileId == 2496225055)
                 {
                     if (mod.Name != ModContext.ModId)
                         SuppressWc = true;
